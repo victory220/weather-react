48798f258a788fe84c0b5bc362ebb9670f56b77a
victory220 <107876583+victory220@users.noreply.github.com>
2022-09-16 21:28:04 +0300

    Added daily forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,16 +2,29 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecastDay.css";
 
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function weekDay(time) {
+  time = time * 1000;
+  let date = new Date(time);
+  let day = date.getDay(date);
+  return days[day];
+}
+
 export default function WeatherForecastDay(props) {
   return (
     <div className="ForecastDay">
       <div className="row">
         <div className="col">
-          <div className="ForecastDay">Thu</div>
-          <WeatherIcon code="01d" size={36} />
+          <div className="ForecastDay">{weekDay(props.data.dt)}</div>
+          <WeatherIcon code={props.data.weather[0].icon} size={36} />
           <div className="ForecastTemp">
-            <span className="ForecastTemp-max">19°</span>
-            <span className="ForecastTemp-min">10°</span>
+            <span className="ForecastTemp-max">
+              {Math.round(props.data.temp.max)}°
+            </span>
+            <span className="ForecastTemp-min">
+              {Math.round(props.data.temp.min)}°
+            </span>
           </div>
         </div>
       </div>
